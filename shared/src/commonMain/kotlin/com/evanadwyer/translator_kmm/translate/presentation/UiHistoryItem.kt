81c37617d583cbab1d0813acb723b4fc2b209881
evanadwyer <evanadwyer@gmail.com>
2024-04-18 10:08:46 -0700

    Shared TranslateViewModel

---
+++ shared/src/commonMain/kotlin/com/evanadwyer/translator_kmm/translate/presentation/UiHistoryItem.kt
@@ -0,0 +1,11 @@
+package com.evanadwyer.translator_kmm.translate.presentation
+
+import com.evanadwyer.translator_kmm.core.presentation.UiLanguage
+
+data class UiHistoryItem(
+    val id: Long,
+    val fromText: String,
+    val toText: String,
+    val fromLanguage: UiLanguage,
+    val toLanguage: UiLanguage
+)
