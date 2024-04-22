c807ae949ad359e406cb6a27f234312cbb11ff8c
evanadwyer <evanadwyer@gmail.com>
2024-04-22 09:57:51 -0700

    Text-to-Speech

--- androidApp/src/main/java/com/evanadwyer/translator_kmm/android/translate/presentation/TranslateScreen.kt
+++ androidApp/src/main/java/com/evanadwyer/translator_kmm/android/translate/presentation/TranslateScreen.kt
@@ -1,5 +1,6 @@
 package com.evanadwyer.translator_kmm.android.translate.presentation
 
+import android.speech.tts.TextToSpeech
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
@@ -21,8 +22,10 @@ import com.evanadwyer.translator_kmm.android.R
 import com.evanadwyer.translator_kmm.android.translate.presentation.components.LanguageDropDown
 import com.evanadwyer.translator_kmm.android.translate.presentation.components.SwapLanguagesButton
 import com.evanadwyer.translator_kmm.android.translate.presentation.components.TranslateTextField
+import com.evanadwyer.translator_kmm.android.translate.presentation.components.rememberTextToSpeech
 import com.evanadwyer.translator_kmm.translate.presentation.TranslateEvent
 import com.evanadwyer.translator_kmm.translate.presentation.TranslateState
+import java.util.Locale
 
 @Composable
 fun TranslateScreen(
@@ -86,6 +89,7 @@ fun TranslateScreen(
             item {
                 val clipboardManager = LocalClipboardManager.current
                 val keyboardController = LocalSoftwareKeyboardController.current
+                val tts = rememberTextToSpeech()
                 TranslateTextField(
                     fromText = state.fromText,
                     toText = state.toText,
@@ -115,7 +119,13 @@ fun TranslateScreen(
                         onEvent(TranslateEvent.CloseTranslation)
                     },
                     onSpeakerClick = {
-
+                        tts.language = state.toLanguage.toLocale() ?: Locale.ENGLISH
+                        tts.speak(
+                            state.toText,
+                            TextToSpeech.QUEUE_FLUSH,
+                            null,
+                            null
+                        )
                     },
                     onTextFieldClick = {
                         onEvent(TranslateEvent.EditTranslation)
