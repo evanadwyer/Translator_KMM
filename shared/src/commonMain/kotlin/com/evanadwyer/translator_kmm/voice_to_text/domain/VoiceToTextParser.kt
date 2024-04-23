a25720aeba20a80fcb77079af4a137b9259c64ea
evanadwyer <evanadwyer@gmail.com>
2024-04-23 11:24:37 -0700

    Speech Recognition on Android

---
+++ shared/src/commonMain/kotlin/com/evanadwyer/translator_kmm/voice_to_text/domain/VoiceToTextParser.kt
@@ -0,0 +1,12 @@
+package com.evanadwyer.translator_kmm.voice_to_text.domain
+
+import com.evanadwyer.translator_kmm.core.domain.util.CommonStateFlow
+
+interface VoiceToTextParser {
+    val state: CommonStateFlow<VoiceToTextParserState>
+
+    fun startListening(languageCode: String)
+    fun stopListening()
+    fun cancel()
+    fun reset()
+}
